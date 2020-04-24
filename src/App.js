cd533adcc282f7236f5568f2a2fee50a3c0f9aaa
Le Van Thao <vanthaodhdt@gmail.com>
2020-04-24 14:57:13 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -4,16 +4,8 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
-import loadable from "@loadable/component";
 
-// import Login from './pages/Login';
-// import Dashboard from './pages/Dashboard';
-// import Setting from './pages/Setting';
-// import Functions from './pages/Functions';
-// import NotFound from './pages/NotFound';
-// import FunctionFoo from './pages/FunctionFoo';
 const RouteWithSubRoutes = (route) => (
   <Route
     exact={route.computedMatch.isExact} 
@@ -23,10 +15,7 @@ const RouteWithSubRoutes = (route) => (
     )}
   />
 );
-const LoadableComponent = loadable(() => import("./pages/Dashboard.js"), {
-  // fallback: <Loading />
-});
-const LoadableLogin = loadable(() => import("./pages/Login"));
+
 function App () {
   return (
     <Router>
@@ -34,13 +23,6 @@ function App () {
           file routes config (khi thêm bớt component thì chỉ sửa file config)
           không cần sửa code tại đây
       */}
-      {/* <LoadableComponent></LoadableComponent>
-      <LoadableLogin></LoadableLogin> */}
-      {
-        // showRoute(routes)
-      }
-      {/* <Link to='/login'></Link>
-      <Link to='/dashboard'></Link> */}
       <Switch>{
         routes.map(route => (
           <RouteWithSubRoutes key={route.path} {...route} />
@@ -49,12 +31,5 @@ function App () {
     </Router>
   )
 }
-// function showRoute(routes) {
-//   let result = null;
-//   result = routes.map((route) => {return (
-//     <Route key={route.path} path={route.path} exact={route.exact} component={route.component} />
-//   )})
-//   return <Switch>{result}</Switch>
-// }
 
 export default App
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,48 +1,67 @@
 // isProtected: các route cần được bảo vệ
 // Có thể thêm giá trị vào đây
-import React from 'react'
-import Login from '../pages/Login';
-import Dashboard from '../pages/Dashboard';
-import Setting from '../pages/Setting';
-import Functions from '../pages/Functions';
-import NotFound from '../pages/NotFound';
-import FunctionFoo from '../pages/FunctionFoo';
-
+import React from 'react';
+import Loadable from 'react-loadable';
+import { Redirect } from 'react-router-dom';
+// import Login from '../pages/Login';
+// import Dashboard from '../pages/Dashboard';
+// import Setting from '../pages/Setting';
+// import Functions from '../pages/Functions';
+// import NotFound from '../pages/NotFound';
+// import FunctionFoo from '../pages/FunctionFoo';
+// const Loading = () => <div></div></div>
+const DynamicImport = (LoaderComponent) => Loadable({
+  loader: LoaderComponent,
+  loading: () => null
+});
+// const Login = loadable(() => import('../pages/Login'));
+// const Dashboard = loadable(() => import('../pages/Dashboard'));
+// const Setting = loadable(() => import('../pages/Setting'));
+// const Functions = loadable(() => import('../pages/Functions'));
+// const FunctionFoo = loadable(() => import('../pages/FunctionFoo'));
+// const NotFound = loadable(() => import('../pages/NotFound'));
 export default [
   {
     title: 'Login Page | Sample App',
-    component: () => <Login></Login>,
+    component: DynamicImport(() => import('../pages/Login')),
     path: '/login',
     isProtected: false
   },
   {
     title: 'Dashboard | Sample App',
-    component: () => <Dashboard></Dashboard>,
+    component: DynamicImport(() => import('../pages/Dashboard')),
     path: '/dashboard',
     isProtected: true,
     exact: true
   },
   {
+    title: 'Dashboard | Sample App',
+    component: () => <Redirect to="/dashboard" />,
+    path: '/',
+    isProtected: true,
+    exact: true
+  },
+  {
     title: 'Setting | Sample App',
-    component: () => <Setting></Setting>,
+    component: DynamicImport(() => import('../pages/Setting')),
     path: '/setting',
     isProtected: true
   },
   {
     title: 'Functions',
-    component: () => <Functions></Functions>,
+    component: DynamicImport(() => import('../pages/Functions')),
     path: '/functions',
     isProtected: true,
   },
   {
     title: 'Functions',
-    component:() => <FunctionFoo></FunctionFoo>,
+    component:DynamicImport(() => import('../pages/FunctionFoo')),
     path: '/functions/foo',
     isProtected: true
   },
   {
     title:'Not found',
-    component: () => <NotFound></NotFound>,
-    path: '*'
+    component: DynamicImport(() => import('../pages/NotFound')),
+    path: '/*'
   }
 ]
